ef70287e9e12989305b30a3518676c3016f11352
Chris <zhris@zhr.is>
2024-12-07 20:00:03 -0600

    Initial Commit

---
+++ PeriodicTable/PeriodicTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeriodicTableApp.swift
+//  PeriodicTable
+//
+//  Created by Chris on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PeriodicTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
